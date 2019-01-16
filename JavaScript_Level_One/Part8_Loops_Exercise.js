ad6405d986939a97c4e0f230b4cca009394d4c6f
kuangwenyi <kuangwinnie@gmail.com>
2019-01-16 11:24:42 -0500

    trivial practice for js

--- JavaScript_Level_One/Part8_Loops_Exercise.js
+++ JavaScript_Level_One/Part8_Loops_Exercise.js
@@ -11,12 +11,18 @@
 // Use a For Loop to print (console.log()) out the word "hello" 5 times.
 //
 // Do this with a While Loop and a For Loop
-
+var temp = "hello";
 // While Loop
-
+var count = 0;
+while(count < 5){
+    count += 1;
+    console.log(temp);
+}
 
 // For Loop
-
+for(count = 0; count < 5; count++){
+    console.log(temp);
+}
 
 
 
@@ -26,10 +32,21 @@
 
 // Use Loops to console.log() (print out) all the odd numbers from 1 to 25
 // Do this using two methods, a while loop and a for loop
-
+var n;
 // METHOD ONE
 // While Loop
-
+n = 0;
+while(n <= 25){
+    if(n % 2 != 0){
+        console.log("odd less than 25 "+n);
+    }
+    n++;
+}
 
 // METHOD TWO
 // For Loop
+for(n =0; n < 26; n++){
+    if(n % 2 != 0){
+        console.log("odd less than 25 "+n);
+    }
+}
---
+++ JavaScript_Level_One/myscript.js
@@ -0,0 +1,44 @@
+var hot = false;
+var temp = 20;
+
+// if (true) {
+//     console.log("I ran")
+// }
+// if (temp > 80){
+//     console.log("Temperature is greater than 29");
+//     hot = true;
+// } else if(temp <= 80 && temp >= 50){
+//     // console.log("It's not very hot today!");
+//     console.log("Average temp Outside");
+// } else if (temp < 50 && temp >= 32){
+//     console.log("It's pretty cold outside.")
+// } else{
+//     console.log("baby it\'s cold outside ")
+// }
+
+// var x = 0;
+
+// while(x < 5){
+//     if(x === 3){
+//         console.log('hey guys the x is: '+x);
+//         break;
+//     }
+//     console.log('x currently is: ' + x);
+//     x += 1;
+// }
+
+// console.log(hot);
+
+// var num = 1;
+
+// while (num < 11){
+//     console.log(num);
+//     num += 1;
+//     if (num % 2 === 0){
+//         console.log('There num ,'+num+' is a even number.');
+//     }
+// }
+
+for (var i = 0; i < 5; i++){
+    console.log("Number is "+i);
+}
