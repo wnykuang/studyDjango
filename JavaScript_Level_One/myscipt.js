854b36c644f8c6f04198ca4353c415fe541c2a9e
kuangwenyi <kuangwinnie@gmail.com>
2019-01-16 08:04:17 -0500

    intro to javascript

---
+++ JavaScript_Level_One/myscipt.js
@@ -0,0 +1,3 @@
+alert('welcome to your bank.')
+var deposit = prompt("How much would you like to .")
+alert("You've deposited: "+deposit)
---
+++ JavaScript_Level_One/p3Solution.js
@@ -0,0 +1,4 @@
+var weightInPound = prompt("What is your weight in pound?")
+var weightInKg = Math.trunc(0.453592 * weightInPound)
+alert("So your weight in kilogram is: "+weightInKg+" kg")
+console.log("Conversion complete!")
