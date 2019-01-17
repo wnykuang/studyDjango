95bc0afcb21a7c432e293585a4489393c85b45ee
kuangwenyi <kuangwinnie@gmail.com>
2019-01-17 08:57:10 -0500

    change the last prompt to alert

--- JavaScript_Level_One/part9project.js
+++ JavaScript_Level_One/part9project.js
@@ -5,7 +5,7 @@ var age = prompt("How old are you?");
 var height = prompt("How tall are you(in centimeter)?");
 var petName = prompt("What's the name of your pet?");
 
-prompt("Thank you for the information.")
+alert("Thank you for the information.")
 
 if (fname[0] == lname[0] && Number(age) > 20 && Number(age) < 30 
  && Number(height) >= 170 && petName[petName.length - 1] === 'y'){
