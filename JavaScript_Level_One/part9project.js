9a3a15b19d870740c79648702f9631208475ee34
kuangwenyi <kuangwinnie@gmail.com>
2019-01-17 08:58:32 -0500

    to "==="

--- JavaScript_Level_One/part9project.js
+++ JavaScript_Level_One/part9project.js
@@ -7,7 +7,7 @@ var petName = prompt("What's the name of your pet?");
 
 alert("Thank you for the information.")
 
-if (fname[0] == lname[0] && Number(age) > 20 && Number(age) < 30 
+if (fname[0] === lname[0] && Number(age) > 20 && Number(age) < 30 
  && Number(height) >= 170 && petName[petName.length - 1] === 'y'){
      console.log("Hey you are a spy!")
  }
