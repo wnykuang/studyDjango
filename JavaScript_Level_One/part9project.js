cdcd79b83be9bc6a29be9b9f56581d76d7ec1569
kuangwenyi <kuangwinnie@gmail.com>
2019-01-17 08:47:46 -0500

    simple practise for js as a project

---
+++ JavaScript_Level_One/part9project.js
@@ -0,0 +1,11 @@
+
+var fname = prompt("What's your first name?");
+var lname = prompt("What's your last name?");
+var age = prompt("How old are you?");
+var tall = prompt("How tall are you(in centimeter)?");
+var petName = prompt("What's the name of your pet?");
+
+if (fname[0] == lname[0] && Number(age) > 20 && Number(age) < 30 
+ && Number(tall) >= 170 && petName[petName.length - 1]){
+     console.log("Hey you are a spy!")
+ }
