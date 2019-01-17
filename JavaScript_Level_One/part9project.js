b6cdc4cf86f24aac6399dc65bad1c554bcf9c851
kuangwenyi <kuangwinnie@gmail.com>
2019-01-17 08:55:44 -0500

    tiny variable name fix

--- JavaScript_Level_One/part9project.js
+++ JavaScript_Level_One/part9project.js
@@ -2,10 +2,12 @@
 var fname = prompt("What's your first name?");
 var lname = prompt("What's your last name?");
 var age = prompt("How old are you?");
-var tall = prompt("How tall are you(in centimeter)?");
+var height = prompt("How tall are you(in centimeter)?");
 var petName = prompt("What's the name of your pet?");
 
+prompt("Thank you for the information.")
+
 if (fname[0] == lname[0] && Number(age) > 20 && Number(age) < 30 
- && Number(tall) >= 170 && petName[petName.length - 1] === 'y'){
+ && Number(height) >= 170 && petName[petName.length - 1] === 'y'){
      console.log("Hey you are a spy!")
  }
