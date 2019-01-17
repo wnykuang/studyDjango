5ba8e79d17fb9a20958fd1c0865b0e1851140012
kuangwenyi <kuangwinnie@gmail.com>
2019-01-17 08:51:10 -0500

    a minor fix for a small bug

--- JavaScript_Level_One/part9project.js
+++ JavaScript_Level_One/part9project.js
@@ -6,6 +6,6 @@ var tall = prompt("How tall are you(in centimeter)?");
 var petName = prompt("What's the name of your pet?");
 
 if (fname[0] == lname[0] && Number(age) > 20 && Number(age) < 30 
- && Number(tall) >= 170 && petName[petName.length - 1]){
+ && Number(tall) >= 170 && petName[petName.length - 1] === 'y'){
      console.log("Hey you are a spy!")
  }
