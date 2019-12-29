b9da9c6501c15ea681f6f84923c249fea4585e9d
wnykuang <kuangwinnie@gmail.com>
2019-12-29 13:14:14 -0500

    change git ignore

--- first_project/first_project/urls.py
+++ first_project/first_project/urls.py
@@ -1,4 +1,4 @@
-"""ProTwo URL Configuration
+"""first_project URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.1/topics/http/urls/
@@ -14,10 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from AppTwo import views
+from django.urls import path, include
+from django.conf.urls import url, include
+from first_app import views
 
 urlpatterns = [
+    path("",  views.index, name="index"),
     path('admin/', admin.site.urls),
-    path('', views.index, name='index')
+    url(r'^first_app/', include('first_app.url'))
 ]
